b4c4133b7501d6e4cc3331634753ecac7c2d42ea
Frank Hoedemakers <frank.hoedemakers@gmail.com>
2022-11-01 15:25:08 +0100

    Added make command

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ cd build || exit 1
 cmake .. -DCMAKE_BUILD_TYPE=RELEASENODEBUG ..
 #/usr/bin/cmake --build /home/pi/projects/pico/pico-infones/build --config Debug -j 6 --
 #/usr/bin/cmake --build /home/pi/projects/pico/pico-infones/build --config Debug --target clean -j 6 --
-#make -j 4
+make -j 4
