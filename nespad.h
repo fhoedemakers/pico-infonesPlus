0b1adcf88599e79bd0f2bf706529f7f44ed78dc2
Phillip Burgess <paintyourdragon@dslextreme.com>
2023-01-21 12:38:15 -0800

    Add NES pad build defs & code, not yet connected

---
+++ nespad.h
@@ -0,0 +1,6 @@
+#pragma once
+
+extern bool nespad_begin(uint32_t cpu_khz, uint8_t clkPin, uint8_t dataPin,
+                         uint8_t latPin);
+extern void nespad_read_start(void);
+extern uint8_t nespad_read_finish(void);
