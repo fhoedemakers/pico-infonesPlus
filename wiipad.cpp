97d54c73ed4fe7d8b1e8fe9b0b421e798f2a2400
Phillip Burgess <paintyourdragon@dslextreme.com>
2023-03-17 18:56:54 -0700

    Wii WIP

--- wiipad.cpp
+++ wiipad.cpp
@@ -5,17 +5,17 @@
 #if WII_PIN_SDA >= 0 and WII_PIN_SCL >= 0
 
 void wiipad_begin(void) {
-    i2c_init(WII_I2C, 400000);
     gpio_set_function(WII_PIN_SDA, GPIO_FUNC_I2C);
     gpio_set_function(WII_PIN_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(WII_PIN_SDA);
     gpio_pull_up(WII_PIN_SCL);
+    i2c_init(WII_I2C, 400000);
     uint8_t init1[] = { 0xF0, 0x55 };
     uint8_t init2[] = { 0xFB, 0x00 };
     sleep_ms(100);
-    i2c_write_timeout_us(WII_I2C, WII_ADDR, init1, 2, true, 10000);
+    i2c_write_timeout_us(WII_I2C, WII_ADDR, init1, 2, true, 100);
     sleep_ms(100);
-    i2c_write_timeout_us(WII_I2C, WII_ADDR, init2, 2, false, 10000);
+    i2c_write_timeout_us(WII_I2C, WII_ADDR, init2, 2, false, 100);
 }
 
 uint8_t wiipad_read(void) {
@@ -31,9 +31,9 @@ uint8_t wiipad_read(void) {
     uint8_t v = 0;
     uint8_t req[] = { 0x00 };
     uint8_t buf[6];
-    i2c_write_timeout_us(WII_I2C, WII_ADDR, req, 1, true, 100000);
+    i2c_write_timeout_us(WII_I2C, WII_ADDR, req, 1, true, 100);
     sleep_us(200);
-    if (i2c_read_timeout_us(WII_I2C, WII_ADDR, buf, sizeof buf, false, 100000) == sizeof buf) {
+    if (i2c_read_timeout_us(WII_I2C, WII_ADDR, buf, sizeof buf, false, 200) == sizeof buf) {
         if (!(buf[5] & 0x01)) v |= UP;
         if (!(buf[4] & 0x40)) v |= DOWN;
         if (!(buf[5] & 0x02)) v |= LEFT;
