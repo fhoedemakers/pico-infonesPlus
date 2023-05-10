46e24e64df7b3c137f028f42e12c763e8abdb686
Frank Hoedemakers <fhoedemakers@hotmail.com>
2023-05-10 16:48:33 +0200

    Fix error when build dir does not exist

--- build_feather_dvi.sh
+++ build_feather_dvi.sh
@@ -10,8 +10,8 @@ cd `dirname $0` || exit 1
 . ./checksdk.sh
 if [ -d build ] ; then
 	rm -rf build || exit 1
-	mkdir build || exit 1
 fi
+mkdir build || exit 1
 cd build || exit 1
 cmake -DCMAKE_BUILD_TYPE=RELEASENODEBUG -DINFONES_PLUS_HW_CONFIG=3 ..
 make -j 4
