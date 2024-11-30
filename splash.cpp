bc2859029e8d2f08128a6e0a0422d3a64a3d7dd8
Frank Hoedemakers <frank.hoedemakers@gmail.com>
2024-11-30 16:38:45 +0100

    Add emulator specific splash screen

---
+++ splash.cpp
@@ -0,0 +1,52 @@
+#include "menu.h"
+#include "FrensHelpers.h"
+
+static int fgcolorSplash = DEFAULT_FGCOLOR;
+static int bgcolorSplash = DEFAULT_BGCOLOR;
+void splash()
+{
+    char s[SCREEN_COLS + 1];
+    ClearScreen(bgcolorSplash);
+    
+    strcpy(s, "Pico-Info");
+    putText(SCREEN_COLS / 2 - (strlen(s) + 4) / 2, 2, s, fgcolorSplash, bgcolorSplash);
+
+    putText((SCREEN_COLS / 2 - (strlen(s)) / 2) + 7, 2, "N", CRED, bgcolorSplash);
+    putText((SCREEN_COLS / 2 - (strlen(s)) / 2) + 8, 2, "E", CGREEN, bgcolorSplash);
+    putText((SCREEN_COLS / 2 - (strlen(s)) / 2) + 9, 2, "S", CBLUE, bgcolorSplash);
+    putText((SCREEN_COLS / 2 - (strlen(s)) / 2) + 10, 2, "+", fgcolorSplash, bgcolorSplash);
+
+    strcpy(s, "NES emulator for RP2040");
+    putText(SCREEN_COLS / 2 - strlen(s) / 2, 3, s, fgcolorSplash, bgcolorSplash);
+    strcpy(s, "Emulator");
+    putText(SCREEN_COLS / 2 - strlen(s) / 2, 5, s, fgcolorSplash, bgcolorSplash);
+    strcpy(s, "@jay_kumogata");
+    putText(SCREEN_COLS / 2 - strlen(s) / 2, 6, s, CLIGHTBLUE, bgcolorSplash);
+
+    strcpy(s, "Pico Port");
+    putText(SCREEN_COLS / 2 - strlen(s) / 2, 9, s, fgcolorSplash, bgcolorSplash);
+    strcpy(s, "@shuichi_takano");
+    putText(SCREEN_COLS / 2 - strlen(s) / 2, 10, s, CLIGHTBLUE, bgcolorSplash);
+
+    strcpy(s, "Menu System & SD Card Support");
+    putText(SCREEN_COLS / 2 - strlen(s) / 2, 13, s, fgcolorSplash, bgcolorSplash);
+    strcpy(s, "@frenskefrens");
+    putText(SCREEN_COLS / 2 - strlen(s) / 2, 14, s, CLIGHTBLUE, bgcolorSplash);
+
+    strcpy(s, "NES/WII controller support");
+    putText(SCREEN_COLS / 2 - strlen(s) / 2, 17, s, fgcolorSplash, bgcolorSplash);
+
+    strcpy(s, "@PaintYourDragon @adafruit");
+    putText(SCREEN_COLS / 2 - strlen(s) / 2, 18, s, CLIGHTBLUE, bgcolorSplash);
+
+    strcpy(s, "PCB Design");
+    putText(SCREEN_COLS / 2 - strlen(s) / 2, 21, s, fgcolorSplash, bgcolorSplash);
+
+    strcpy(s, "@johnedgarpark");
+    putText(SCREEN_COLS / 2 - strlen(s) / 2, 22, s, CLIGHTBLUE, bgcolorSplash);
+
+    strcpy(s, "https://github.com/");
+    putText(SCREEN_COLS / 2 - strlen(s) / 2, 25, s, CLIGHTBLUE, bgcolorSplash);
+    strcpy(s, "fhoedemakers/pico-infonesPlus");
+    putText(1, 26, s, CLIGHTBLUE, bgcolorSplash);
+}
