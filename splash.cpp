a25f781c1c696c29bfde6ef12d2439f309d89931
Frank Hoedemakers <frank.hoedemakers@gmail.com>
2024-12-26 12:42:36 +0100

    Update splash screen text

--- splash.cpp
+++ splash.cpp
@@ -19,7 +19,7 @@ void splash()
     putText((SCREEN_COLS / 2 - (strlen(s)) / 2) + 9, 2, "S", CBLUE, bgcolorSplash);
     putText((SCREEN_COLS / 2 - (strlen(s)) / 2) + 10, 2, "+", fgcolorSplash, bgcolorSplash);
 
-    strcpy(s, "NES emulator for RP2040");
+    strcpy(s, "NES emulator for RP2040/2350");
     putText(SCREEN_COLS / 2 - strlen(s) / 2, 3, s, fgcolorSplash, bgcolorSplash);
     strcpy(s, "Emulator");
     putText(SCREEN_COLS / 2 - strlen(s) / 2, 5, s, fgcolorSplash, bgcolorSplash);
@@ -51,5 +51,5 @@ void splash()
     strcpy(s, "https://github.com/");
     putText(SCREEN_COLS / 2 - strlen(s) / 2, 25, s, CLIGHTBLUE, bgcolorSplash);
     strcpy(s, "fhoedemakers/pico-infonesPlus");
-    putText(1, 26, s, CLIGHTBLUE, bgcolorSplash);
+    putText(SCREEN_COLS / 2 - strlen(s) / 2, 26, s, CLIGHTBLUE, bgcolorSplash);
 }
