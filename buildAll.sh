bbc84a45dbf5017bcda172a1049ac605e346f45b
Frank Hoedemakers <frank.hoedemakers@gmail.com>
2025-04-08 14:18:31 +0200

    Update hardware configurations Adafruit FruitJam in buildAll.sh

--- buildAll.sh
+++ buildAll.sh
@@ -27,13 +27,13 @@ do
 	./bld.sh -c $HWCONFIG -w
 done
 # build for Pico 2 -arm-s
-HWCONFIGS="1 2"
+HWCONFIGS="1 2 5"
 for HWCONFIG in $HWCONFIGS
 do
 	./bld.sh -c $HWCONFIG -2
 done
 # build for Pico 2 -riscv
-HWCONFIGS="1 2"
+HWCONFIGS="1 2 5"
 if [ ! -d $PICO_SDK_PATH/toolchain/RISCV_RPI_2_0_0_2/bin ] ; then
 	echo "RISC-V toolchain not found in $PICO_SDK_PATH/toolchain/RISCV_RPI_2_0_0_2/bin"	
 	echo "To install the RISC-V toolchain, execute \"bld.sh -h\" for instructions"
