5489b341b8180fbcf52ee8b0f2b271e960e87a2c
Frank Hoedemakers <frank.hoedemakers@gmail.com>
2025-11-25 17:58:04 +0100

    Add SLOTFORMAT definition for save state file paths

--- state.h
+++ state.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "InfoNES_Types.h"
 #define SAVESTATEDIR "/SAVESTATES"
+#define SLOTFORMAT SAVESTATEDIR "/%s/%08X/slot%d.sta"
 // Maximum number of save state slots
 #define MAXSAVESTATESLOTS 5
 int Emulator_SaveState(const char* path);
