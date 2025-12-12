188225d06a2e879c4e78921dec3de48ed6c0c91d
Frank Hoedemakers <frank.hoedemakers@gmail.com>
2025-12-12 14:29:36 +0100

    Moved MAXSAVESTATESLOTS to menu.h

--- state.h
+++ state.h
@@ -1,7 +1,5 @@
 #pragma once
 #include "InfoNES_Types.h"
 
-// Maximum number of save state slots
-#define MAXSAVESTATESLOTS 5
 int Emulator_SaveState(const char* path);
 int Emulator_LoadState(const char* path);
