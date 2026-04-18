eb39198270b0e6e3ea6f8d1688146c5b0262e6ef
Frank Hoedemakers <frank.hoedemakers@gmail.com>
2026-04-18 08:17:39 +0200

    Add reset game functionality to emulator controls

--- main.cpp
+++ main.cpp
@@ -64,6 +64,7 @@ static uint32_t CPUFreqKHz = EMULATOR_CLOCKFREQ_KHZ;
 // Order must match enum in menu_options.h
 const int8_t g_settings_visibility_nes[MOPT_COUNT] = {
     0,                               // Exit Game, or back to menu. Always visible when in-game.
+    0,                               // Reset Game
     0,                               // Save / Restore State
     !HSTX,                           // Screen Mode (only when not HSTX)
     HSTX,                            // Scanlines toggle (only when HSTX)
@@ -285,6 +286,7 @@ static DWORD prevButtons[2]{};
 static int rapidFireMask[2]{};
 static int rapidFireCounter = 0;
 static bool reset = false;
+static bool resetGame = false;
 void InfoNES_PadState(DWORD *pdwPad1, DWORD *pdwPad2, DWORD *pdwSystem)
 {
     static constexpr int LEFT = 1 << 6;
@@ -488,7 +490,7 @@ void InfoNES_PadState(DWORD *pdwPad1, DWORD *pdwPad2, DWORD *pdwSystem)
     {
         saveNVRAM();
     }
-    *pdwSystem = reset ? PAD_SYS_QUIT : 0;
+    *pdwSystem = (reset || resetGame) ? PAD_SYS_QUIT : 0;
 }
 
 void InfoNES_MessageBox(const char *pszMsg, ...)
@@ -907,6 +909,9 @@ int InfoNES_LoadFrame()
             quickSaveAction = SaveStateTypes::NONE;
            
         }
+        if (rval == 5) {
+           resetGame = true;
+        }
     }
     if (loadSaveStateMenu) {
         if (quickSaveAction == SaveStateTypes::LOAD_AND_START) {
@@ -1155,7 +1160,7 @@ int main()
             printf("Playing selected ROM from menu: %s\n", selectedRom);
         }
 #endif
-        reset = loadSaveStateMenu = false;
+        reset = resetGame = loadSaveStateMenu = false;
         //EXT_AUDIO_MUTE_INTERNAL_SPEAKER(settings.flags.fruitJamEnableInternalSpeaker == 0);
         EXT_AUDIO_SETVOLUME(settings.fruitjamVolumeLevel);
         *ErrorMessage = 0;
@@ -1181,8 +1186,11 @@ int main()
         } else {
             printf("No auto-save configured for this ROM.\n");
         }
-        romSelector_.init(ROM_FILE_ADDR);
-        InfoNES_Main();
+        do {
+            resetGame = false;
+            romSelector_.init(ROM_FILE_ADDR);
+            InfoNES_Main();
+        } while (resetGame);
 #if !EMBEDDED_NES_ROM
         selectedRom[0] = 0;
 #endif
